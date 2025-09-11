102600ac4b295227adca6f451371d97454f1f9da
viveksharmatws <viveksharma.tws@gmail.com>
2025-09-11 10:32:01 +0530

    voice clone ui fixes

--- src/components/VoiceCloning.jsx
+++ src/components/VoiceCloning.jsx
@@ -20,20 +20,24 @@ const VoiceCloning = () => {
   ];
 
   return (
-    <section className="flex flex-col items-start  min-h-screen p-4">
+    <section className="flex flex-col items-start  min-h-screen p-2 md:!p-4">
       {/* Title */}
-      <h2 className="text-xl sm:text-2xl font-inter font-semibold text-white text-center mb-24">
+      <h2 className="text-xl sm:text-2xl font-inter font-semibold text-white text-center mb-12 md:!mb-24">
         Clone Your Voice
       </h2>
       <div className="w-full max-w-[633px] mx-auto space-y-10">
         {/* Upload Section */}
         <div className="flex flex-col items-center justify-center">
-          <h2 className="text-white font-inter text-2xl font-light mb-6">
+          <h2 className="text-white font-inter text-xl md:!text-2xl font-light mb-6">
             Clone a Voice
           </h2>
           <div className="bg-[#43434333] rounded-xl border border-[#2A2A2A] p-6 text-center cursor-pointer hover:border-[#8267EC] transition max-w-[320px] w-full">
             <div className="bg-[#101010] px-4 py-3 w-fit h-fit rounded-md mb-5 mx-auto">
-              <img src="/upload.svg" alt="upload" className="h-8 w-8  mx-auto" />
+              <img
+                src="/upload.svg"
+                alt="upload"
+                className="h-8 w-8  mx-auto"
+              />
             </div>
             <p className="text-white font-inter text-sm sm:text-base font-light">
               Click To Upload, or Drag And Drop
@@ -49,44 +53,40 @@ const VoiceCloning = () => {
           <h2 className="text-lg sm:text-xl font-inter font-light text-white mb-4 ">
             My Voice
           </h2>
-
           <div className="space-y-4">
             {voices.map((voice, index) => (
               <div
                 key={index}
-                className="flex items-center justify-between bg-transparent rounded-lg"
+                className="grid grid-cols-[auto_1fr_auto] sm:grid-cols-[auto_1fr_auto] grid-cols-1 sm:items-center gap-2 md:!gap-3 bg-transparent rounded-lg cursor-pointer"
               >
-                {/* Left: Avatar + Info */}
-                <div className="flex items-center gap-3">
-                  <img
-                    src={voice.img}
-                    alt={voice.name}
-                    className="h-10 w-10 rounded-full object-cover"
-                  />
-                  <div className="flex gap-2 items-center">
-                    <p className="text-white font-inter font-medium text-sm sm:text-base">
-                      {voice.name}
-                    </p>
-                    <p className="text-white/60 text-xs sm:text-sm">
-                      {voice.description}
-                    </p>
-                  </div>
+                <img
+                  src={voice.img}
+                  alt={voice.name}
+                  className="h-8 w-8 md:!h-10 md:!w-10 rounded-full object-cover"
+                />
+
+                <div className="flex gap-2 items-center">
+                  <p className="text-white font-inter font-medium text-xs sm:!text-base">
+                    {voice.name}
+                  </p>
+                  <p className="text-white/60 text-xs sm:!text-sm">
+                    {voice.description}
+                  </p>
                 </div>
 
-                {/* Right: Actions */}
-                <div className="flex items-center gap-3">
+                <div className="flex items-center gap-1 md:!gap-3 justify-end sm:col-auto col-span-1">
                   <button>
                     <img
                       src="/play.svg"
                       alt="play"
-                      className="h-6 w-6 cursor-pointer"
+                      className="h-5 w-5 md:!h-6 md:!w-6 cursor-pointer"
                     />
                   </button>
                   <button>
                     <img
                       src="/delete.svg"
                       alt="delete"
-                      className="h-6 w-6 cursor-pointer"
+                      className="h-5 w-5 md:!h-6 md:!w-6 cursor-pointer"
                     />
                   </button>
                 </div>
